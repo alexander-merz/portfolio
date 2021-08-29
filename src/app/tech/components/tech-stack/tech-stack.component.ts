41e79823993c3b16f73cfb2dee64b515a8fadd9a
alexander-merz <web.engineering.merz@gmail.com>
2021-08-29 20:16:45 +0200

    minor style changes

--- src/app/tech/components/tech-stack/tech-stack.component.ts
+++ src/app/tech/components/tech-stack/tech-stack.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'cv-tech-stack',
   templateUrl: './tech-stack.component.html',
   styleUrls: ['./tech-stack.component.scss']
 })
-export class TechStackComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class TechStackComponent {}
