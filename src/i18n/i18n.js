60656990e57d13b850198c5f72a799ecd5f5e6fe
alexander-merz <web.engineering.merz@gmail.com>
2022-12-14 19:07:17 +0100

    react to languagechange

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,8 +1,22 @@
-const language = navigator.language.includes("de") ? "de" : "en"
-const translations = await import(`./i18n.${language}.json`).then((m) => m.default)
+const initialTranslations = await import(`./i18n.${navigator.language}.json`).then((m) => m.default)
 const localizationTargets = document.querySelectorAll("[data-translate]")
 
-for (const target of localizationTargets) {
-  const key = target.getAttribute("data-translate") || target.textContent
-  target.textContent = translations[key.toUpperCase()]
+const translationMap = { [navigator.language]: initialTranslations }
+
+function translate() {
+  for (const target of localizationTargets) {
+    const key = target.getAttribute("data-translate") || target.textContent
+    target.textContent = translationMap[navigator.language][key.toUpperCase()]
+  }
 }
+
+window.addEventListener("languagechange", async () => {
+  if (!Object.keys(translationMap).includes(navigator.language)) {
+    const translations = await import(`./i18n.${navigator.language}.json`).then((m) => m.default)
+    translationMap[navigator.language] = translations
+  }
+
+  translate()
+})
+
+translate()
