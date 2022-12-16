dc8170fc06e9d6d2c4350e0d4dcfa6735014c2a6
alexander-merz <web.engineering.merz@gmail.com>
2022-12-16 20:17:28 +0100

    move markdown translation logic to i18n.js

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,30 +1,37 @@
 const preferredLanguage = getPreferredLanguage()
 const initialTranslations = await import(`./i18n.${preferredLanguage}.json`).then((m) => m.default)
-const localizationTargets = document.querySelectorAll("[data-translate]")
 const translationMap = { [preferredLanguage]: initialTranslations }
-const toggle = document.querySelector("#language-toggle")
+
+const localizationTargets = document.querySelectorAll('[data-translate]')
+const languageToggle = document.querySelector('#language-toggle')
+const markdowns = document.querySelectorAll('wc-markdown')
 
 function getPreferredLanguage() {
-  return localStorage.getItem("language-preference") ?? navigator.language.slice(0, 2)
+  return localStorage.getItem('language-preference') ?? navigator.language.slice(0, 2)
 }
 
 function toggleLanguage(language) {
-  return language.slice(0, 2).toLowerCase() === "en" ? "de" : "en"
+  return language.slice(0, 2).toLowerCase() === 'en' ? 'de' : 'en'
 }
 
 function translate(language) {
-  toggle.textContent = toggleLanguage(getPreferredLanguage()).toUpperCase()
+  languageToggle.textContent = toggleLanguage(getPreferredLanguage()).toUpperCase()
 
   for (const target of localizationTargets) {
     let key
 
-    if (target.getAttribute("data-translate")) {
-      key = target.getAttribute("data-translate")
+    if (target.getAttribute('data-translate')) {
+      key = target.getAttribute('data-translate')
       target.textContent = translationMap[language][key.toUpperCase()]
-    } else if (target.getAttribute("data-translate-tooltip") && target.getAttribute("data-tooltip")) {
-      key = target.getAttribute("data-translate-tooltip")
-      target.setAttribute("data-tooltip", translationMap[language][key.toUpperCase()])
+    } else if (target.getAttribute('data-translate-tooltip') && target.getAttribute('data-tooltip')) {
+      key = target.getAttribute('data-translate-tooltip')
+      target.setAttribute('data-tooltip', translationMap[language][key.toUpperCase()])
     }
+  }
+
+  for (const markdown of markdowns) {
+    const [name] = markdown.src.split('.')
+    markdown.src = `${name}.${language}.md`
   }
 }
 
@@ -37,11 +44,11 @@ async function switchLanguage(language) {
   translate(language)
 }
 
-toggle.addEventListener("click", () => {
-  localStorage.setItem("language-preference", toggleLanguage(getPreferredLanguage()))
+languageToggle.addEventListener('click', () => {
+  localStorage.setItem('language-preference', toggleLanguage(getPreferredLanguage()))
   switchLanguage(getPreferredLanguage())
 })
 
-window.addEventListener("languagechange", () => switchLanguage(getPreferredLanguage()))
+window.addEventListener('languagechange', () => switchLanguage(getPreferredLanguage()))
 
 translate(getPreferredLanguage())
--- src/main.js
+++
@@ -1,8 +0,0 @@
-const markdowns = document.querySelectorAll("wc-markdown")
-
-if (navigator.language.includes("de")) {
-  for (const markdown of Array.from(markdowns)) {
-    const [name] = markdown.src.split(".")
-    markdown.src = `${name}.de.md`
-  }
-}
