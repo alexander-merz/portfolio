7762a211de23cb666838497115cdcc9a10b5d64a
alexander-merz <web.engineering.merz@gmail.com>
2023-06-20 09:18:48 +0200

    optimize for print media

--- src/scripts/date-time.js
+++ src/scripts/date-time.js
@@ -2,18 +2,32 @@
 const currentJob = document.querySelector('#current-job')
 const currentJobStartDate = Date.parse(currentJob.querySelector('time').getAttribute('datetime'))
 const timeSpentMs = Date.now() - currentJobStartDate
-const timeSpentYears = (timeSpentMs / 1000 / 60 / 60 / 24 / 365).toFixed(1)
+const timeSpentMonths = Math.floor(timeSpentMs / 1000 / 60 / 60 / 24 / 30.4375)
 
-// Create element containing translation of "years"
-const yearsElement = document.createElement('span')
-yearsElement.setAttribute('data-translate', 'years')
-yearsElement.textContent = 'Years'
+const years = Math.floor(timeSpentMonths / 12)
+const months = timeSpentMonths % 12
 
-// Create element containing the caluclated time spent at current job
+// Create element containing the calculated time spent at current job
 const timeSpentYearsElement = document.createElement('span')
 timeSpentYearsElement.id = 'current-job-duration'
-timeSpentYearsElement.textContent = timeSpentYears.toString().concat(' ')
-timeSpentYearsElement.append(yearsElement)
+
+// Create element containing translation of "months"
+const monthsElement = document.createElement('span')
+monthsElement.setAttribute('data-translate', months > 1 ? 'months' : 'month')
+
+if (years > 0) {
+  // Create element containing translation of "years"
+  const yearsElement = document.createElement('span')
+  yearsElement.setAttribute('data-translate', years > 1 ? 'years' : 'year')
+
+  // Create element containing translation of "and"
+  const andElement = document.createElement('span')
+  andElement.setAttribute('data-translate', 'and')
+
+  timeSpentYearsElement.append(years.toString(), ' ', yearsElement, ' ', andElement, ' ')
+}
+
+timeSpentYearsElement.append(months.toString(), ' ', monthsElement)
 
 // Insert the time spent element after the "today" element
 currentJob.querySelector('time[data-translate="today"]').after(timeSpentYearsElement)
